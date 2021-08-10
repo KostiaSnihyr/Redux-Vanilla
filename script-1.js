997afce935ca830e520922dc9a0bb528f3467c8d
Kostiantyn <kosssm155@gmail.com>
2021-08-10 14:21:19 +0300

    createStore

--- script-1.js
+++ script-1.js
@@ -1,1 +1,51 @@
-console.log(1);
+// reducer
+function todos(state = [], action) {
+	if (action.type === 'ADD_TODO') {
+		return state.concat([action.todo]);
+	}
+
+	return state;
+}
+
+// store
+function createStore() {
+	let state;
+	let listeners = [];
+
+	const getState = () => state;
+
+	const subscribe = listener => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter(l !== listener);
+		};
+	};
+
+	const dispatch = action => {
+		state = todos(state, action);
+		listeners.forEach(listener => listener());
+	};
+
+	return {
+		getState,
+		subscribe,
+		dispatch,
+	};
+}
+
+const store = createStore();
+
+console.log(store);
+
+const unsbscribe = store.subscribe(() => {
+	console.log('The new state is: ', store.getState());
+});
+
+store.dispatch({
+	type: 'ADD_TODO',
+	todo: {
+		id: 0,
+		name: 'Learn Redux',
+		complete: false,
+	},
+});
